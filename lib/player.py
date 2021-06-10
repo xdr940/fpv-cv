2aa983ab94f5bc3d44ce718adc228c6162a73e1f
xdr940 <xdr940@126.com>
2021-06-10 17:07:52 +0800

    fix bugs

--- lib/player.py
+++ lib/player.py
@@ -31,6 +31,8 @@ def openCvPlay(fifo = './fifo_file'):
         try:
             _, img = cap.read()
             # img = cv2.imread(fifo)
+            #tag =pass(img)
+            # img = tag +img
             cv2.imshow('img', img)
 
             if cv2.waitKey(1) & 0xff == ord('q'):
@@ -40,7 +42,8 @@ def openCvPlay(fifo = './fifo_file'):
 
 
 if __name__ == '__main__':
-    cmd = "/usr/bin/ffplay -fs -i ./fifo_file -analyzeduration 1 -probesize 32 -sync ext"
-    FFPlayer(cmd)
-    while True:
-        pass
+    # cmd = "/usr/bin/ffplay -fs -i ./fifo_file -analyzeduration 1 -probesize 32 -sync ext"
+    # FFPlayer(cmd)
+    # while True:
+    #     pass
+    openCvPlay()
--- play.py
+++ play.py
@@ -1,7 +1,6 @@
-from .lib.player import openCvPlay
+from lib.player import openCvPlay
 
-def main():
-    openCvPlay()
 
 if __name__ == '__main__':
-    main()
+
+    openCvPlay()
